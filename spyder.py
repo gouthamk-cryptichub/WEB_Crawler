8f0b63639d5eb99f35d500837bea1ad0c88db0a7
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-05 05:31:06 -0400

    Complete webcrawler with on wordlist

--- spyder.py
+++ spyder.py
@@ -3,17 +3,24 @@ import requests
 import re
 import urlparse
 
-target_website = "google.com"
+target_website = "altoromutual.com"
 
 target_URL = "http://" + target_website
-foubd_links = []
-def get_links(website):
-    page = requests.get(target_URL).content
+found_links = []
+
+def get_links(webpage):
+    page = requests.get(webpage).content
     return re.findall('(?:href=")(.*?)"', page)
 
-links = get_links(target_website)
-for url in links:
-    url = urlparse.urljoin(target_URL, url)
-    if target_website.split(".")[0] in url and url not in found_links and "#" not in url:
-        foubd_links.append(url)
-        print(url)
+def seek_page(target_page):
+    links = get_links(target_page)
+    for url in links:
+        url = urlparse.urljoin(target_page, url)
+        if target_website.split(".")[0] in url and url not in found_links and "#" not in url:
+            found_links.append(url)
+            print("[+] " + url)
+            seek_page(url)
+
+print("[...] Starting crawler...\n")
+seek_page(target_URL)
+print("\n[+] Found all Possible links on target.")
