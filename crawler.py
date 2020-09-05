11003bf2481e3fc883f32c7ff0220077b34e8705
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-05 05:53:01 -0400

    Final0 (Added Command line args)

--- crawler.py
+++ crawler.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 import requests
+import optparse
+import argparse
+
+def get_args():
+    try:
+        parser = optparse.OptionParser()
+        parser.add_option("-t", "--target", dest="target_website", help="Target website example.com")
+        value, options = parser.parse_args()
+    except:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-t", "--target", dest="target_website", help="Target website example.com")
+        value = parser.parse_args()
+    if not value.target_website:
+        parser.error("[-] ERROR Missing argument, use --help or more info")
+    else:
+        return value
 
 def seek(url):
     try:
@@ -7,16 +23,26 @@ def seek(url):
     except:
         pass
 
-target_website = "google.com"
-with open ("subdomainlist.txt", "r") as wordlist:
-    for word in wordlist:
-        seek_url = word.strip() + "." + target_website
-        resp = seek(seek_url)
-        if resp:
-            print("[+] Detected Subdomain --> " + seek_url)
-            with open("directory.txt", "r") as word_list:
-                for wrd in word_list:
-                    seek_dir = seek_url + "/" + wrd.strip()
-                    resp_dir = seek(seek_dir)
-                    if resp_dir:
-                        print("\t[+] Found Page --> " + seek_dir)
+def run(target_website):
+    with open ("subdomainlist.txt", "r") as wordlist:
+        for word in wordlist:
+            seek_url = word.strip() + "." + target_website
+            resp = seek(seek_url)
+            if resp:
+                print("[+] Detected Subdomain --> " + seek_url)
+                with open("directory.txt", "r") as word_list:
+                    for wrd in word_list:
+                        seek_dir = seek_url + "/" + wrd.strip()
+                        resp_dir = seek(seek_dir)
+                        if resp_dir:
+                            print("\t[+] Found Page --> " + seek_dir)
+
+
+value = get_args()
+print("[...] Starting crawler...\n")
+try:
+    run(value.target_website)
+except KeyboardInterrupt:
+    print("\n[-] Detected CTRL + C, Stopping Crawler...")
+    exit()
+print("\n[+] Found all Possible links on target.")
