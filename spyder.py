52165a482aa7311cd24ccd5d5b8a15f8384dae98
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-05 05:07:43 -0400

    Spidering thr traget home page.

--- spyder.py
+++ spyder.py
@@ -4,8 +4,9 @@ import re
 import urlparse
 
 target_website = "google.com"
+
 target_URL = "http://" + target_website
-organisation = target_website.split(".")[0]
+foubd_links = []
 def get_links(website):
     page = requests.get(target_URL).content
     return re.findall('(?:href=")(.*?)"', page)
@@ -13,5 +14,6 @@ def get_links(website):
 links = get_links(target_website)
 for url in links:
     url = urlparse.urljoin(target_URL, url)
-    if organisation in url:
+    if target_website.split(".")[0] in url and url not in found_links and "#" not in url:
+        foubd_links.append(url)
         print(url)
