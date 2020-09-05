708975fd6ca808c6a40942aba49b8f7cf3cceeb0
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-05 05:44:45 -0400

    Final0 (Added Command line args)

--- spyder.py
+++ spyder.py
@@ -2,25 +2,44 @@
 import requests
 import re
 import urlparse
+import optparse
+import argparse
 
-target_website = "altoromutual.com"
+def get_args():
+    try:
+        parser = optparse.OptionParser()
+        parser.add_option("-t", "--target", dest="target_website", help="Target website example.com")
+        value, options = parser.parse_args()
+    except:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-t", "--target", dest="target_website", help="Target website example.com")
+        value = parser.parse_args()
+    if not value.target_website:
+        parser.error("[-] ERROR Missing argument, use --help or more info")
+    else:
+        return value
 
-target_URL = "http://" + target_website
+value = get_args()
+
+target_URL = "http://" + value.target_website
 found_links = []
 
 def get_links(webpage):
     page = requests.get(webpage).content
     return re.findall('(?:href=")(.*?)"', page)
-
 def seek_page(target_page):
     links = get_links(target_page)
     for url in links:
         url = urlparse.urljoin(target_page, url)
-        if target_website.split(".")[0] in url and url not in found_links and "#" not in url:
+        if value.target_website.split(".")[0] in url and url not in found_links and "#" not in url:
             found_links.append(url)
             print("[+] " + url)
             seek_page(url)
 
 print("[...] Starting crawler...\n")
-seek_page(target_URL)
+try:
+    seek_page(target_URL)
+except KeyboardInterrupt:
+    print("\n[-] Detected CTRL + C, Stopping Crawler...")
+    exit()
 print("\n[+] Found all Possible links on target.")
